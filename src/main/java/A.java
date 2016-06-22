dfcdcce223c29d7379db6a047593cc5a9c77bef1
Grigory Rubstein <grigoryr@liveperson.com>
2016-06-22 17:38:36 +0300

    added tesseract dependecy

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -3,12 +3,14 @@
  */
 
 import java.io.File;
+import java.net.URISyntaxException;
+
 import net.sourceforge.tess4j.*;
 
 public class A {
 
-    public static void main(String[] args) {
-        File imageFile = new File("/Users/rotems/dev/IdeaProjects/sensorme/src/main/resources/4_360.jpg");
+    public static void main(String[] args) throws URISyntaxException {
+        File imageFile = new File(ClassLoader.getSystemResource("4_360.jpg").toURI());
         ITesseract instance = new Tesseract();  // JNA Interface Mapping
         // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
         instance.setDatapath("/usr/local/share/tessdata");
