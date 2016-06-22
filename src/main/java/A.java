c2d910edba34a5bfd7499b8b4040d552d594ad6d
rotems <rotems@liveperson.com>
2016-06-22 17:27:03 +0300

    hello world

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,5 +1,22 @@
 /**
  * Created by rotems on 6/22/16.
  */
+
+import java.io.File;
+import net.sourceforge.tess4j.*;
+
 public class A {
+
+    public static void main(String[] args) {
+        File imageFile = new File("/Users/rotems/dev/IdeaProjects/sensorme/src/main/resources/4_360.jpg");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
